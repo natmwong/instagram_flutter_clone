7f568f056dd96a5a8b9fba1734eacdae8b5bb96c
Natasha <wong.natasha.m@gmail.com>
2024-06-18 12:10:42 -0700

    first commit

---
+++ lib/responsive/responsive_layout_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ResponsiveLayout extends StatelessWidget(
+  const ResponsiveLayout({Key? key}) : super(key: key);
+)
+
+@override
+Widget build(BuildContext context) {
+  return LayoutBuilder(
+    builder: (context, constraints) {
+      if(constraints.maxWidth > )
+    },
+  )
+}
