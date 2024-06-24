9b38513137fe6b262ed0d8f4b371a3874ea7a1a9
Natasha <wong.natasha.m@gmail.com>
2024-06-24 12:11:41 -0700

    Created user model

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -2,6 +2,7 @@ import "dart:typed_data";
 import "package:cloud_firestore/cloud_firestore.dart";
 import "package:firebase_auth/firebase_auth.dart";
 import "package:flutter/material.dart";
+import "package:instagram_flutter/models/user.dart" as model;
 import "package:instagram_flutter/resources/storage_methods.dart";
 
 class AuthMethods {
@@ -31,16 +32,21 @@ class AuthMethods {
         String photoUrl = await StorageMethods()
             .uploadImageToStorage('profilePics', file, false);
 
+        model.User user = model.User(
+          username: username,
+          uid: cred.user!.uid,
+          email: email,
+          bio: bio,
+          followers: [],
+          following: [],
+          photoUrl: photoUrl,
+        );
+
         //add user to our database
-        await _firestore.collection('users').doc(cred.user!.uid).set({
-          'username': username,
-          'uid': cred.user!.uid,
-          'email': email,
-          'bio': bio,
-          'followers': [],
-          'following': [],
-          'photoUrl': photoUrl,
-        });
+        await _firestore
+            .collection('users')
+            .doc(cred.user!.uid)
+            .set(user.toJson());
 
         //this method produces a diff uid in the doc and collection
         // await _firestore.collection('users').add({
