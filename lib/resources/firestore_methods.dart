3b8c828ca33cb4322a9c25db1577d024f9d59a77
Natasha <wong.natasha.m@gmail.com>
2024-07-05 18:21:26 -0700

    Implemented storing comments into Firebase

--- lib/resources/firestore_methods.dart
+++ lib/resources/firestore_methods.dart
@@ -52,7 +52,35 @@ class FirestoreMethods {
         });
       }
     } catch (e) {
-      print(e.toString(),);
+      print(
+        e.toString(),
+      );
+    }
+  }
+
+  Future<void> postComment(
+      String postId, String text, String uid, String name, String profilePic) async {
+    try {
+      if (text.isNotEmpty) {
+        String commentId = const Uuid().v1();
+        _firestore
+            .collection('posts')
+            .doc(postId)
+            .collection('comments')
+            .doc(commentId)
+            .set({
+          'profilePic': profilePic,
+          'name': name,
+          'uid': uid,
+          'text': text,
+          'commentId': commentId,
+          'datePublished': DateTime.now(),
+        });
+      } else {
+        print('Text is empty');
+      }
+    } catch (e) {
+      print(e.toString());
     }
   }
 }
